a92635e2d9fe8c829b70b5251afb13cb8ace0b8a
Brageskrettingland <brageskrettingland@gmail.com>
2023-02-25 21:01:47 +0100

    removed some code that wasnt in use

--- include/obstacle.hpp
+++ include/obstacle.hpp
@@ -20,33 +20,30 @@ public:
         width = w_1;
         length = l_1;
     }
-    obstacle(float l,float w){
-        width = w;
-        length = l;
-    }
-    auto create_geometry(float length,float height,float width);
+
+    auto create_geometry(float length,float height,float width, float pos);
 private:
 
     float width = 0;
     float height = 1;
     float length = 0;
+    float pos = 0;
 };
 
 std::array<float,3> return_color(){
     float random_red = math::randomInRange(0,1) ;
     float random_green = math::randomInRange(0,1) ;
     float random_blue = math::randomInRange(0,1) ;
-    std::cout <<  random_blue << " " << random_green<< "" << random_red <<std::endl;
     std::array<float,3> random_colors = {random_red, random_green, random_blue};
     return random_colors;
 };
-auto obstacle::create_geometry (float l,float h,float w){
+auto obstacle::create_geometry (float l,float h,float w, float p){
     auto geometry = BoxGeometry::create(l,h,w);
     auto material = MeshBasicMaterial::create();
     std::array<float,3>random_color  = return_color();
     material->color = Color(random_color[0],random_color[1],random_color[2]);
     auto mesh = Mesh::create(geometry, material);
-    mesh->position.x = 0;
+    mesh->position.x = p;
     return mesh;
 }
 
--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace threepp;
 
 int main() {
-    obstacle test(1,2);
+    obstacle test;
     Canvas canvas;
     GLRenderer renderer(canvas);
     renderer.setClearColor(Color::aliceblue);
@@ -19,27 +19,9 @@ int main() {
 
     auto group = Group::create();
     scene->add(group);
-    group->add(test.create_geometry(1,1,3));
-    group->add(test.create_geometry(3,1,1));
-    {
-        //    auto geometry = BoxGeometry::create();
-        //    auto material = MeshBasicMaterial::create();
-        //  material->color = Color::blue;
-        // auto mesh = Mesh::create(geometry, material);
-        //mesh->position.x = 1;
-        //  group->add(mesh);
+    group->add(test.create_geometry(1,1,3,1));
+    group->add(test.create_geometry(1,1,3,-1));
 
-    }
-
-    {
-   //    auto geometry = BoxGeometry::create();
-    //    auto material = MeshBasicMaterial::create();
-      //  material->color = Color::blue;
-       // auto mesh = Mesh::create(geometry, material);
-        //mesh->position.x = 1;
-      //  group->add(mesh);
-
-    }
 
     renderer.enableTextRendering();
     auto& textHandle = renderer.textHandle("Hello World");
