ae8b450f82c46a406d1d26351f2cb11e3f89bf02
Brageskrettingland <brageskrettingland@gmail.com>
2023-05-09 10:09:37 +0200

    added a getScore function and expandend the Unit tests for gameLogic.hpp

--- tests/test_myGameLogic.cpp
+++ tests/test_myGameLogic.cpp
@@ -18,30 +18,4 @@ TEST_CASE("UpdateHexColor test") {
     CHECK(hexColor == refrenceColor);
 }
 
-TEST_CASE("GameLogic updateGame movement and score") {
-    sphere player(0);
-    auto playerTest = player.mesh();
-    auto group1 = Group::create();
-    auto group2 = Group::create();
-    float dt = 1.0f;
-    Canvas canvas;
-    GLRenderer renderer(canvas);
-    auto &textHandle = renderer.textHandle();
-
-    bool hasCollision = false;
-
-    GameLogic gameLogic;
-    gameLogic.updateGame(playerTest, group1, group2, dt, textHandle, hasCollision);
-
-    CHECK(group1->position.z != 0.0f);
-    CHECK(group2->position.z != 0.0f);
-
-
-    playerTest->position.x = 10*3;
-    gameLogic.updateGame(playerTest, group1, group2, dt, textHandle, hasCollision);
-    int score = gameLogic.getScore();
-    CHECK(score == playerTest->position.x/3);
-
-}
-
 
